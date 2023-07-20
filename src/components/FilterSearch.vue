ddeab1535aa32af96a9a1f0c1a40a3eee8a55b70
MioRain <ericzero1593@hotmail.com>
2023-07-21 04:27:44 +0800

    feat: add filter feature

--- src/components/FilterSearch.vue
+++ src/components/FilterSearch.vue
@@ -1,13 +1,146 @@
 <script setup>
+import { onMounted, reactive, ref, inject } from 'vue';
+import { apiHelper } from '../utils/helpers'
+
+const filter = inject('filter');
+const fetchProducts = inject('fetchProducts')
+const state = reactive({
+  categories: []
+})
+const name = ref(''),
+  minPrice = ref(''),
+  maxPrice = ref(''),
+  minSize = ref(''),
+  maxSize = ref('')
+
+const setCategory = (categoryId) => {
+  filter.categoryId = categoryId
+  fetchProducts()
+}
+
+const setFilter = (key, value) => {
+  filter[key] = value
+}
+
+const fetchCategories = async () => {
+  const { data } = await apiHelper.get('/categories')
+  state.categories = data
+}
+
+onMounted(() => {
+  fetchCategories()
+})
 
 </script>
 
 <template>
-<div class="category">
-  <h1>categories</h1>
-</div>
+  <ul class="categories">
+    <li @click="setCategory(null)">
+      <span class="category" :class="{ active: !filter.categoryId }">all</span>
+    </li>
+    <li v-for="category in state.categories" :key="category.id">
+      <span>|</span>
+      <span @click="setCategory(category.id)" class="category" :class="{ active: filter.categoryId === category.id }">{{
+        category.name }}</span>
+    </li>
+  </ul>
+  <div class="filter">
+    <div class="price">
+      <label for="">
+        價格
+        <input @input="setFilter('minPrice', minPrice)" v-model="minPrice" type="number" min="0">
+        <span>-</span>
+        <input @input="setFilter('maxPrice', maxPrice)" v-model="maxPrice" type="number" min="0">
+      </label>
+    </div>
+    <div class="size">
+      <label for="">
+        尺寸
+        <input @input="setFilter('minSize', minSize)" v-model="minSize" type="number" min="0">
+        <span>-</span>
+        <input @input="setFilter('maxSize', maxSize)" v-model="maxSize" type="number" min="0">
+      </label>
+    </div>
+  </div>
+  <div class="search-bar">
+    <input @input="setFilter('name', name)" v-model="name" type="text">
+    <button @click="fetchProducts">Search</button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+input,
+button {
+  padding: 0 10px;
+  border-radius: 5px;
+  background-color: white;
+}
 
+.categories {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+
+  .category {
+    text-decoration: underline;
+    cursor: pointer;
+
+    &.active {
+      font-weight: bold;
+      text-decoration: none;
+      cursor: none;
+    }
+  }
+
+  li {
+    color: rgb(8, 146, 200);
+
+    span:not(.category) {
+      margin: 0 10px;
+    }
+  }
+}
+
+.filter {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+
+  label {
+    margin-left: 20px;
+
+    input {
+      width: 80px;
+    }
+
+    span {
+      margin: 0 10px;
+    }
+  }
+
+  .size input {
+    width: 60px;
+  }
+
+}
+
+.search-bar {
+  height: 35px;
+  margin-top: 20px;
+  padding-left: 20px;
+  display: flex;
+  justify-content: center;
+
+  input {
+    width: 23rem;
+    border-radius: 10px 0 0 10px;
+  }
+
+  button {
+    width: 5rem;
+    border-radius: 0 10px 10px 0;
+    background-color: var(--color-dark-1);
+    cursor: pointer;
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,50 @@
 <script setup>
+import { reactive, provide, onMounted } from 'vue'
+import { apiHelper } from '../utils/helpers'
 import HeaderArea from '../components/HeaderArea.vue'
 import FilterSearch from '../components/FilterSearch.vue'
 
+const data = reactive({
+  products: []
+})
+const filter = reactive({
+  name: '',
+  categoryId: null,
+  minSzie: null,
+  maxSize: null,
+  minPrice: null,
+  maxPrice: null
+});
+
+const fetchProducts = async () => {
+  const response = await apiHelper.get('/products', {
+    params: {
+      filter
+    }
+  })
+  data.products = response.data
+}
+
+provide('filter', filter)
+provide('fetchProducts', fetchProducts)
+
+onMounted(() => {
+  fetchProducts()
+})
 </script>
 
 <template>
   <HeaderArea />
-  
-  <main>
-    <h1>main</h1>
-    <FilterSearch />
-  </main>
 
+  <main>
+    <FilterSearch />
+    <p v-for="product in data.products">{{ product.name }}</p>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 main {
-  height: 100vh;
+  height: 100%;
   padding: 0 100px;
   padding-top: 100px;
   background-color: var(--color-light-1);
