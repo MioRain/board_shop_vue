9c150dd740d1e031f6c5382fcb262d599ae3563a
MioRain <ericzero1593@hotmail.com>
2023-07-21 15:30:48 +0800

    feat: add product view

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,3 +1,36 @@
+<script setup>
+import { reactive, onMounted } from 'vue'
+import { useRoute } from 'vue-router';
+import { apiHelper } from '../utils/helpers'
+import HeaderArea from '../components/HeaderArea.vue'
+
+const route = useRoute()
+const data = reactive({
+  product: {}
+})
+
+const fetchProduct = async (productId) => {
+  const response = await apiHelper.get('/products/' + productId)
+  data.product = response.data
+}
+
+onMounted(() => {
+  fetchProduct(route.params.id)
+})
+</script>
+
 <template>
-  <h1>product page</h1>
+  <HeaderArea />
+
+  <main>
+    <h1>{{ data.product.id }} | {{ data.product.name }}</h1>
+    
+  </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  padding: 0 100px;
+  padding-top: 100px;
+}
+</style>
